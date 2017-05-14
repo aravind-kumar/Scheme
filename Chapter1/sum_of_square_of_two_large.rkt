6458e4f21706f4c80a766be711fc5a476f393576
aravind-kumar <karavindkumar1993@gmail.com>
2017-05-14 15:17:14 +0530

    Added comments for the program

--- Chapter1/sum_of_square_of_two_large.rkt
+++ Chapter1/sum_of_square_of_two_large.rkt
@@ -12,9 +12,9 @@
 ;Function to find the largest two numbers of a given three numbers x,y,z and 
 ;computes the sum of squares
 (define (sum-of-square-of-largest-two x y z) ;:-> number?
-        (if ( > x y) 
-            (sum-of-square x (max y z))
-            (sum-of-square y (max x z))))
+        (if ( > x y)                      ; Check if x is greater than y
+            (sum-of-square x (max y z))   ; If so x is not the smallest number,so find the max(y,z) 
+            (sum-of-square y (max x z)))) ; else y is not the smallest,so find the max(x,z) to find the highest
 
 (sum-of-square-of-largest-two 2 3 4)
          
