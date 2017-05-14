7e6f325f7a4da7e9def52e668369e982350be158
aravind-kumar <karavindkumar1993@gmail.com>
2017-05-14 15:14:11 +0530

    Added comments for the program

--- Chapter1/sum_of_square_of_two_large.rkt
+++ Chapter1/sum_of_square_of_two_large.rkt
@@ -1,9 +1,17 @@
 #! /usr/bin/env/racket
 #lang racket
 
-(define (square x) (* x x))
-(define (sum-of-square x y) (+ (square x) (square y)))
-(define (sum-of-square-of-largest-two x y z)
+;@ Title: Given 3 numbers the following program computes the sum of largest two. 
+
+;Helper Function to compute the square of given input x
+(define (square x) (* x x)) ;:-> number?
+
+;Helper Function to compute the sum of square of given two inputs x and y.
+(define (sum-of-square x y) (+ (square x) (square y))) ;:-> number?
+
+;Function to find the largest two numbers of a given three numbers x,y,z and 
+;computes the sum of squares
+(define (sum-of-square-of-largest-two x y z) ;:-> number?
         (if ( > x y) 
             (sum-of-square x (max y z))
             (sum-of-square y (max x z))))
